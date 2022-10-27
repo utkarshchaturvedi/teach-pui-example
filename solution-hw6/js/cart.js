30614858bc9dcd5d810a8a3a92b06ba2e86f5fb5
utkarshchaturvedi <utkarsh.c27@gmail.com>
2022-10-27 14:33:14 -0400

    Updated HW6

--- solution-hw6/js/cart.js
+++ solution-hw6/js/cart.js
@@ -1,22 +1,4 @@
-//HW5
-
-
-//Set to store rolls . Use local storage
-let cart = new Set();
-const cartList = new Set;
-if(localStorage.getItem("cart")) productList = Array.from(JSON.parse(localStorage.getItem("cart"))); 
-
-
-//Objects for glazing prices
-const glazingPrices= {
-    'Original': 0, 'Sugar Milk': 0, 'Vanilla Milk': 0.50, 'Double Chocolate': 1.50
-};
-
-//Objects for pack size prices
-const packSizePrices = {
-    '1': 1, '3': 3, '6': 5, '12': 10
-}
-
+let cart = JSON.parse(localStorage.getItem('cart'));
 
 //creating a Roll class
 class Roll {
@@ -25,148 +7,89 @@ class Roll {
         this.glazing =  rollGlazing;
         this.size = packSize;
         this.basePrice = basePrice;
-        this.element = null;
-        const newPrice = (this.basePrice + parseFloat(glazingPrices[this.glazing])) * parseFloat(packSizePrices[this.size]);
-        this.totalPrice = parseFloat(newPrice.toFixed(2));
+        this.calculatedPrice = this.calculatedPrice;
     }
 }
 
-//Function to save to local storage
-function saveToLocalStorage() {
-    const cartString = JSON.stringify(cart);
-    localStorage.setItem("cartStorage", cartString);
-    console.log(cartString);
+//Objects for glazing prices
+let glazingPrices= {
+    'Keep Original': 0, 
+    'Sugar Milk': 0,
+    'Vanilla Milk': 0.50, 
+    'Double Chocolate': 1.50
+};
+
+//Objects for pack size prices
+let packSizePrices = {
+    '1': 1, 
+    '3': 3, 
+    '6': 5, 
+    '12': 10
 }
 
-function retrieveFromLocalStorage() {
-    const cartString = localStorage.getItem("cartStorage");
-    const cartArray = JSON.parse(cartString);
-    cart = cartArray;
-    console.log(cart);
+//Initializing glazing and pack prices
+let glazePrice = 0; 
+let packPrice = 1;
+
+//function to update the total price
+function updateCartPrice() {
+    let totalPrice = 0;
+    for (i=0; i<cart.length; i++) {
+        totalPrice = totalPrice + parseFloat(cart[i].calculatedPrice);
+    }
+    const cartPriceElem = document.querySelector('#total-price');
+    cartPriceElem.innerText = "$" + totalPrice.toFixed(2);
 }
 
-if(localStorage.getItem("cartStorage")!= null){
-    retrieveFromLocalStorage();
-}
-
-
-
-//function to add a new object of class Roll
-
-function addNewItem(rollType, rollGlazing, packSize, basePrice) {
-    const roll = new Roll(rollType, rollGlazing, packSize, basePrice);
-    cartList.add(roll);
-    return roll;
-}
-
-const originalroll = addNewItem(
-    'Original',
-    'Sugar Milk',
-    "1",
-    2.49,
-  )
-
-const walnutroll = addNewItem(
-    'Walnut',
-    'Vanilla Milk',
-    "12",
-    3.49
-)
-
-const raisinroll = addNewItem(
-    'Raisin',
-    'Sugar Milk',
-    "3",
-    2.99
-)
-
-const appleroll = addNewItem(
-    'Apple',
-    'Original',
-    "3",
-    3.49
-)
-
-
-function calculatePrice(roll) {
-    let glazingPrice = Object.keys(glazing).find((key) => glazing[key] === roll.glazing);
-    let packPrice = Object.keys(packSizePrices).find((key) => packSizePrices[key] === roll.packSize);
-    //const newPrice = (roll.basePrice + glazingPrice) * packPrice;
-    const newPrice = (roll.basePrice + parseFloat(glazingPrices[roll.glazing])) * parseFloat(packSizePrices[roll.size]);
-    roll.totalPrice = parseFloat(newPrice.toFixed(2));
-    console.log(roll.totalPrice);
+//function to delete the roll 
+function deleteItem(cartItemElem, roll) {
+    cartItemElem.remove();
+    //setting an index for element to be removed
+    let i = cart.indexOf(roll);
+    cart.splice(i, 1); 
+    localStorage.setItem('cart', JSON.stringify(cart));
+    //Call function to update cart price 
+    updateCartPrice();
+    //console.log('Updated Cart', cart);
 }
 
 
-
-//Creating a loop to add new items to cart
-
-for(const roll of cartList) {
-    console.log(roll);
-    createElement(roll);
-}
-
-
-//Creating a function to add items to cart page with a Template
-
-function createElement(roll) {
+//A for loop to add items to the cart
+for (i=0; i<cart.length; i++) {
     const template = document.querySelector('#item-template');
     const clone = template.content.cloneNode(true);
-    roll.element = clone.querySelector('.cart-item');
-    //Appendiung the roll element to the cart list
+    cartItemElem = clone.querySelector('.cart-item');
+
+    //Function to delete a roll item
+    const btnDelete = cartItemElem.querySelector('.delete');
+    btnDelete.addEventListener('click',()=> {  deleteItem(cartItemElem, roll); } );
+
+    let roll = cart[i];
+
+    //Appending the roll element to the cart list
     const cartListElement = document.querySelector('.cart-list');
-    cartListElement.append(roll.element);
+    cartListElement.append(cartItemElem);
 
-    //delete roll function
-    const btnDelete = roll.element.querySelector('.delete');
-    btnDelete.addEventListener('click',()=> { deleteItem(roll) } );
+    //Updating roll data
+    const rollImageElem = cartItemElem.querySelector('.cart-image');
+    rollImageElem.src = "Assets/" + rolls[cart[i].type].imageFile;
 
+    const rollTypeElem = cartItemElem.querySelector('#roll-name');
+    rollTypeElem.innerText = cart[i].type + ' ' + "Cinnamon Roll";
 
-    updateElement(roll);
-    calculatePrice(roll);
-    cartTotal();
+    const glazingElem = cartItemElem.querySelector('#glazing');
+    glazingElem.innerText = "Glazing" + ' ' + cart[i].glazing;
 
+    const packSizeElem = cartItemElem.querySelector('#pack-size');
+    packSizeElem.innerText = "Pack Size: " + cart[i].size; 
+
+    let rollPrice = (cart[i].basePrice + glazingPrices[cart[i].glazing])*packSizePrices[cart[i].size];
+    console.log("z" + glazingPrices[cart[i].glazing]);
+    cart[i].calculatedPrice = rollPrice.toFixed(2);
+    console.log(rollPrice);
+    const rollPriceElement = cartItemElem.querySelector('#roll-price');
+    rollPriceElement.innerText = "$" + cart[i].calculatedPrice;
 }
-
-//Function to update cartItems
-
-function updateElement(roll){
-    const rollImageElem = roll.element.querySelector('.cart-image');
-    rollImageElem.src = "Assets/" + rolls[roll.type].imageFile;
-
-    const rollTypeElem = roll.element.querySelector('#roll-name');
-    rollTypeElem.innerText = roll.type + ' ' + "Cinnamon Roll";
-
-    const glazingElem = roll.element.querySelector('#glazing');
-    glazingElem.innerText = "Glazing" + ' ' + roll.glazing;
-
-    const packSizeElem = roll.element.querySelector('#pack-size');
-    packSizeElem.innerText = "Pack Size: " + roll.size; 
-
-    const rollPriceElem = roll.element.querySelector('#roll-price');
-    rollPriceElem.innerText = '$' + roll.totalPrice;
-}
-
-
-//Calculate cartTotal
-function cartTotal(){
-    const cartTotalPrice = document.querySelector('#totalPrice');
-    let priceTotal = 0;
-    for (const roll of cartList) {
-        priceTotal += roll.totalPrice;
-    }
-    //console.log(priceTotal);
-    cartTotalPrice.innerText = '$' + ' ' + (priceTotal.toFixed(2));
-}
-
-
-//Delete function
-function deleteItem(roll) {
-    roll.element.remove();
-    cart.splice(cart.indexOf(roll), 1);
-    cartList.delete(roll);
-    saveToLocalStorage();
-    console.log(cart);
-    cartTotal();
-}
-
+  
+// update total price for the bill
+updateCartPrice();
--- solution-hw6/js/script.js
+++ solution-hw6/js/script.js
@@ -1,5 +1,14 @@
 //Creating classes for Glaze types and Pack sizes with constructors 
 
+class Roll {
+    constructor(rollType, rollGlazing, packSize, basePrice) {
+        this.type = rollType;
+        this.glazing =  rollGlazing;
+        this.size = packSize;
+        this.basePrice = basePrice;
+    }
+}
+
 class glaze{
 
     glazeType;
@@ -26,10 +35,10 @@ class Pack{
 
 //Creating objects of the class 'Glaze' and 'Pack' with values as prescribed in the HW doc
 
-let keepOriginal = new glaze('Keep original', 0);
-let sugarMilk = new glaze('Sugar milk', 0);
+let keepOriginal = new glaze('Keep Original', 0);
+let sugarMilk = new glaze('Sugar Milk', 0);
 let vanillaMilk =  new glaze('Vanilla Milk', 0.5);
-let doubleChocolate = new glaze('Double chocolate', 1.50);
+let doubleChocolate = new glaze('Double Chocolate', 1.50);
 
 let size1 = new Pack(1,1);
 let size3 = new Pack(3,3);
@@ -61,48 +70,42 @@ for(i=0; i< allPacks.length; i++){
     packSelect.appendChild(selected);
 }
 
-let basePrice= 2.49;
+
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString);
+const rollType = params.get('roll');
+console.log(rollType);
+
+let basePrice= rolls[rollType].basePrice;
+
+document.getElementById('price').innerHTML = '$' + basePrice;
+document.querySelector(".title-wrapper h1").innerText= rollType + " Cinnamon Roll";
+document.querySelector(".product-image-large").src= "Assets/" + rolls[rollType].imageFile;
+document.querySelector("#price").innerText="$"+ rolls[rollType].basePrice;
 
 //Function to update price
 function priceUpdate(element) {
     const price = document.getElementById("price");
     let newPrice = ((basePrice + allGlazes[glazeSelect.selectedIndex].glazePrice)*allPacks[packSelect.selectedIndex].packPrice).toFixed(2);
-    console.log(newPrice)
+    console.log(newPrice);
     // Change the price in html with the newPrice
     price.innerHTML = "$" + newPrice;
 }
 
+//Dynamically changing content 
 
-//Dynamically changing content (HW4 + HW6 Local storage Update)
-
-
-let cart = [];
-if(localStorage.getItem("cart"))cart = Array.from(JSON.parse(localStorage.getItem("cart")));
-
-class Roll {
-    constructor(rollType, rollGlazing, packSize, basePrice) {
-        this.type = rollType;
-        this.glazing =  rollGlazing;
-        this.size = packSize;
-        this.basePrice = basePrice;
-    }
-}
-
-
-const queryString = window.location.search;
-const params = new URLSearchParams(queryString);
-const rollType = params.get('roll');
-
-document.querySelector(".title-wrapper h1").innerText= rollType + " Cinnamon Roll";
-document.querySelector(".product-image-large").src= "Assets/" + rolls[rollType].imageFile;
-document.querySelector("#price").innerText="$"+ rolls[rollType].basePrice;
-
-
-//Add item to cart local storage
 function addToCart(){
+    let cart = [];
+
+    if(localStorage.getItem('cart')) {
+        cart = JSON.parse(localStorage.getItem('cart'));
+    }
+
     let RollInfo= new Roll(rollType, allGlazes[glazeSelect.selectedIndex].glazeType, allPacks[packSelect.selectedIndex].packSize, rolls[rollType].basePrice);
     cart.push(RollInfo);
-    // Converting the cart to a atring and saving it to local storage 
-    localStorage.setItem('cart', JSON.stringify(cart));
     console.log(cart);
+
+    localStorage.setItem('cart', JSON.stringify(cart));
 }
+
+
